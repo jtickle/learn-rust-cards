546d839fe8aab226f13eb71f675e8c1137456124
Jeff Tickle <jeff.tickle@protonmail.com>
2021-12-05 14:25:47 -0500

    Board display

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use rand_pcg::Pcg64;
 
 // Seedable RNGs... "A simple number" looks pretty good
 // https://rust-random.github.io/book/guide-seeding.html
-/// Re-Implemented Cards::shuffle in cards.rs from deckofcards crate to produce a reproducible, seeded shuffle
+/// Re-Implemented Cards::shuffle in cards.rs from deckofcards crate to
+/// produce a reproducible, seeded shuffle
 fn predictable_shuffle(cards: &mut [Card], seed: u64) {
     let mut rng = Pcg64::seed_from_u64(seed);
     // Knuth shuffle
@@ -53,15 +54,36 @@ impl Default for Freecell {
 impl Freecell {
     pub fn new() -> Self { Self::default() }
 
-    pub fn from_deck(deck: &mut Deck) -> Self {
-        let mut board: Self = Self::new();
-        board.deal_from(deck);
-        board
+    //pub fn from_deck(deck: &mut Deck) -> Self {
+    //    let mut board: Self = Self::new();
+    //    board.deal_from(deck);
+    //    board
+    //}
+//
+    //pub fn deal_from(&mut self, deck: &mut Deck) {
+    //    for
+    //    //TODO loop over deck and deal to fields
+    //}
+
+    pub fn get_homecell_topcard(&self, cell: usize) -> Option<Card> {
+        let l = self.homecell[cell].len();
+        if l > 0 {
+            Some(self.homecell[cell].cards[l-1])
+        } else {
+            None
+        }
     }
 
-    pub fn deal_from(&mut self, deck: &mut Deck) {
-        for
-        //TODO loop over deck and deal to fields
+    pub fn get_freecell_card(&self, cell: usize) -> Option<Card> {
+        return self.freecell[cell]
+    }
+
+    pub fn get_field_card_at(&self, field: usize, depth: usize) -> Option<Card> {
+        if self.field[field].len() > depth {
+            Some(self.field[field].cards[depth])
+        } else {
+            None
+        }
     }
 
     pub fn show_homecell(&self, i: usize) -> Option<String> {
@@ -88,17 +110,24 @@ impl Freecell {
     }
 }
 
+fn card_to_str_or(maybe: Option<Card>, def: &str) -> String{
+    match maybe {
+        Some(card) => card.to_str().clone(),
+        None => String::from(def)
+    }
+}
+
 fn print_freecell_board(board: &Freecell) {
 
-    println!("  {} {} {} {}  👑  {} {} {} {}  ", 
-        board.show_homecell(0).unwrap_or(String::from("[]")),
-        board.show_homecell(1).unwrap_or(String::from("[]")),
-        board.show_homecell(2).unwrap_or(String::from("[]")),
-        board.show_homecell(3).unwrap_or(String::from("[]")),
-        board.show_freecell(0).unwrap_or(String::from("[]")),
-        board.show_freecell(1).unwrap_or(String::from("[]")),
-        board.show_freecell(2).unwrap_or(String::from("[]")),
-        board.show_freecell(3).unwrap_or(String::from("[]")),
+    println!(" {} {} {} {}  👑  {} {} {} {} ", 
+        card_to_str_or(board.get_homecell_topcard(0), "[]"),
+        card_to_str_or(board.get_homecell_topcard(1), "[]"),
+        card_to_str_or(board.get_homecell_topcard(2), "[]"),
+        card_to_str_or(board.get_homecell_topcard(3), "[]"),
+        card_to_str_or(board.get_freecell_card(0), "[]"),
+        card_to_str_or(board.get_freecell_card(1), "[]"),
+        card_to_str_or(board.get_freecell_card(2), "[]"),
+        card_to_str_or(board.get_freecell_card(3), "[]"),
     );
 
     let mut maxlen: usize = 1;
@@ -112,16 +141,39 @@ fn print_freecell_board(board: &Freecell) {
     
     for i in 0..maxlen {
         println!(" {}  {}  {}  {}  {}  {}  {}  {} ",
-            board.show_field(0, i).unwrap_or(String::from("  ")),
-            board.show_field(1, i).unwrap_or(String::from("  ")),
-            board.show_field(2, i).unwrap_or(String::from("  ")),
-            board.show_field(3, i).unwrap_or(String::from("  ")),
-            board.show_field(4, i).unwrap_or(String::from("  ")),
-            board.show_field(5, i).unwrap_or(String::from("  ")),
-            board.show_field(6, i).unwrap_or(String::from("  ")),
-            board.show_field(7, i).unwrap_or(String::from("  ")),
+            card_to_str_or(board.get_field_card_at(0, i), "  "),
+            card_to_str_or(board.get_field_card_at(1, i), "  "),
+            card_to_str_or(board.get_field_card_at(2, i), "  "),
+            card_to_str_or(board.get_field_card_at(3, i), "  "),
+            card_to_str_or(board.get_field_card_at(4, i), "  "),
+            card_to_str_or(board.get_field_card_at(5, i), "  "),
+            card_to_str_or(board.get_field_card_at(6, i), "  "),
+            card_to_str_or(board.get_field_card_at(7, i), "  "),
         )
     }
+}
+
+fn suit_to_uc_outline(suit: &Suit) -> char {
+    match suit {
+        deckofcards::Suit::Hearts => '♥',
+        deckofcards::Suit::Spades => '♠',
+        deckofcards::Suit::Diamonds => '♦',
+        deckofcards::Suit::Clubs => '♣',
+    }
+}
+
+fn suit_to_uc_fill(suit: &Suit) -> char {
+    match suit {
+        deckofcards::Suit::Hearts => '♥',
+        deckofcards::Suit::Spades => '♠',
+        deckofcards::Suit::Diamonds => '♦',
+        deckofcards::Suit::Clubs => '♣',
+    }
+}
+
+fn show_card(card: &Card) -> String {
+    // TODO: cli display configuration
+    format!("{}{}", card.rank.to_char(), suit_to_uc_fill(&card.suit))
 }
 
 fn main() {
@@ -134,7 +186,7 @@ fn main() {
     predictable_shuffle(deck.mut_cards(), 42);
 
     for (i, card) in deck.cards().iter().enumerate() {
-        println!("{:02} Card: name is '{:?}', short is {}", i, card.name(), card.to_str());
+        println!("{:02} Card: name is '{}', short is {}", i, card.name(), show_card(card));
     }
 
     let mut board = Freecell::new();
