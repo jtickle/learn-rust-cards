7c235e48525ad0bbb443211dccdfc1b0d66222bb
Jeff Tickle <jeff.tickle@protonmail.com>
2021-12-04 22:14:45 -0500

    Starting to show a board

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,60 @@ fn predictable_shuffle(cards: &mut [Card], seed: u64) {
     }
 }
 
+pub struct Freecell {
+    pub homecell: [Hand; 4],
+    pub freecell: [Option<Card>; 4],
+    pub field: [Hand; 8],
+}
+
+impl Default for Freecell {
+    fn default() -> Self {
+        Self {
+            homecell: [
+                Hand::new(),
+                Hand::new(),
+                Hand::new(),
+                Hand::new(),
+            ],
+            freecell: [
+                None,
+                None,
+                None,
+                None,
+            ],
+            field: [
+                Hand::new(),
+                Hand::new(),
+                Hand::new(),
+                Hand::new(),
+                Hand::new(),
+                Hand::new(),
+                Hand::new(),
+                Hand::new(),
+            ],
+        }
+    }
+}
+
+impl Freecell {
+    pub fn new() -> Self { Self::default() }
+
+    pub fn show_homecell(&self, i: usize) -> Option<String> {
+        if self.homecell[i].len() > 0 {
+            Some(self.homecell[i].cards[0].to_str().clone())
+        } else {
+            None
+        }
+    }
+
+    pub fn show_freecell(&self, i: usize) -> Option<String> {
+        match self.freecell[i] {
+            Some(x) => Some(x.to_str().clone()),
+            None => None,
+        }
+    }
+}
+
 fn main() {
 
     // Create a new deck of cards
@@ -27,4 +81,18 @@ fn main() {
     for (i, card) in deck.cards().iter().enumerate() {
         println!("{:02} Card: name is '{:?}', short is {}", i, card.name(), card.to_str());
     }
+
+    let mut board = Freecell::new();
+
+    println!("  {} {} {} {}  👑  {} {} {} {}  ", 
+        board.show_homecell(0).unwrap_or(String::from("[]")),
+        board.show_homecell(1).unwrap_or(String::from("[]")),
+        board.show_homecell(2).unwrap_or(String::from("[]")),
+        board.show_homecell(3).unwrap_or(String::from("[]")),
+        board.show_freecell(0).unwrap_or(String::from("[]")),
+        board.show_freecell(1).unwrap_or(String::from("[]")),
+        board.show_freecell(2).unwrap_or(String::from("[]")),
+        board.show_freecell(3).unwrap_or(String::from("[]")),
+    );
+    //println!(" {}  {}  {}  {}  {}  {}  {}  {} ")
 }
